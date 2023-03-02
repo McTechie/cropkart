1be7a2cc8d53e6c04d697cee6466e2c054fae4cf
riyashukla6102 <71920274+riyashukla6102@users.noreply.github.com>
2023-03-02 23:09:49 +0530

    Ticket table ui complete

---
+++ admin-dashboard/pages/tickets/index.tsx
@@ -0,0 +1,48 @@
+interface TicketInfo {
+    id: number;
+    type: string;
+    message: string; 
+  }
+  
+  const TicketInfo: TicketInfo[] = [
+    { id: 1, type: 'Ship 1', message: 'Container Ship'},
+    { id: 1, type: 'Ship 1', message: 'Container Ship'},
+    { id: 1, type: 'Ship 1', message: 'Container Ship'},
+    { id: 1, type: 'Ship 1', message: 'Container Ship'},
+    { id: 1, type: 'Ship 1', message: 'Container Ship'},
+    { id: 1, type: 'Ship 1', message: 'Container Ship'},
+
+  ];
+  
+  const TicketTable: React.FC ticketInfo> = () => {
+    return (
+        <>
+        <div className="mx-10">
+        <h1 className="text-white text-2xl font-semibold mb-10" >Ticket Information</h1>
+        <table className="bg-slate-50 rounded-md min-w-full ">
+            <thead>
+            <tr>
+                <th className="border-b px-4 py-2">User ID</th>
+                <th className="border-b px-4 py-2">User Type</th>
+                <th className="border-b px-4 py-2">Message</th>
+                
+            </tr>
+            </thead>
+            <tbody>
+            {ticketInfo.map((ticket) => (
+                <tr key={ticket.id} className="hover:bg-gray-200">
+                <td className="px-4 py-2">{ticket.id}</td>
+                <td className="px-4 py-2">{ticket.type}</td>
+                <td className="px-4 py-2">{ticket.message}</td>
+               
+                </tr>
+            ))}
+            </tbody>
+        </table>
+        </div>
+      </>
+    );
+   
+  };
+  
+  export default TicketTable;
