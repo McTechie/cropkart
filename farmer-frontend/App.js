bfbbe741d1b04be4705caf0b3e28cb4f6f4bd047
techatikin <nikitakhabya03@gmail.com>
2023-03-03 00:04:55 +0530

    feat: initial setup

--- farmer-frontend/App.js
+++ farmer-frontend/App.js
@@ -1,44 +1,21 @@
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { HomeScreen, RestaurantScreen, BasketScreen, PreparingOrderScreen, OrderConfirmedScreen } from './src/screens';
-import { Provider } from 'react-redux';
-import { store } from './src/app/store';
+import { HomeScreen } from './src/screens';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <TailwindProvider>
-        <NavigationContainer>
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen 
-              name='Home'
-              component={HomeScreen}
-            />
-            <Stack.Screen 
-              name='Restaurant'
-              component={RestaurantScreen}
-            />
-            <Stack.Screen 
-              name='Basket'
-              component={BasketScreen}
-              options={{ presentation: 'modal' }}
-            />
-            <Stack.Screen 
-              name='PreparingOrder'
-              component={PreparingOrderScreen}
-              options={{ presentation: 'fullScreenModal' }}
-              />
-            <Stack.Screen 
-              name='OrderConfirmed'
-              component={OrderConfirmedScreen}
-              options={{ presentation: 'fullScreenModal' }}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </TailwindProvider>
-    </Provider>
+    <TailwindProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen
+            name='Home'
+            component={HomeScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </TailwindProvider>
   );
 }
--- farmer-frontend/src/screens/index.js
+++ farmer-frontend/src/screens/index.js
@@ -1,13 +1,1 @@
-import BasketScreen from './BasketScreen';
-import HomeScreen from './HomeScreen';
-import OrderConfirmedScreen from './OrderConfirmedScreen';
-import PreparingOrderScreen from './PreparingOrderScreen';
-import RestaurantScreen from './RestaurantScreen';
-
-export {
-  BasketScreen,
-  HomeScreen,
-  OrderConfirmedScreen,
-  PreparingOrderScreen,
-  RestaurantScreen
-};
+export { default as HomeScreen } from './HomeScreen'
