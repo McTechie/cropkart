328bf03aa98d613a06a8fc856fd95c82da0668ca
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2023-03-03 18:15:13 +0530

    final celery configs
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

---
+++ flask-backend/worker/__init__.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from celery import Celery
+from datetime import timedelta
+from celery.schedules import crontab
+
+
+
+def make_celery(app):
+    celery = Celery(app.import_name, backend=app.config['CELERY_BACKEND'],
+                    broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
+
+app = Flask(__name__)
+app.config['CELERY_BACKEND'] = "redis://redis:6379/0"
+app.config['CELERY_BROKER_URL'] = "redis://redis:6379/0"
+
+app.config['CELERYBEAT_SCHEDULE'] = {
+    'say-every-5-seconds': {
+        'task': 'call_scrapper',
+        'schedule': crontab(hour=12, minute=44),
+    },
+}
+
+
+app.config['CELERY_TIMEZONE'] = 'UTC'
+celery_app = make_celery(app)
+
+
+@celery_app.task(name='call_scrapper')
+def call_scrapper():
+    print ('something')
