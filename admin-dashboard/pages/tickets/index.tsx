0c5f71d88893885aa828d54e4f7e454b2ef44843
Janhavi <patil.janhavi1207@gmail.com>
2023-03-04 08:47:55 +0530

    feat: changed ticket update logic

--- admin-dashboard/pages/tickets/index.tsx
+++ admin-dashboard/pages/tickets/index.tsx
@@ -7,15 +7,15 @@ interface TicketInfo {
     resolved: boolean;
   }
   
-  const TicketInfo: TicketInfo[] = [
-    { id: 1, type: 'Ship 1', message: 'Container Ship', resolved: false},
-    { id: 1, type: 'Ship 1', message: 'Container Ship', resolved: true},
-    { id: 1, type: 'Ship 1', message: 'Container Ship', resolved: false},
-    { id: 1, type: 'Ship 1', message: 'Container Ship', resolved: false},
-    { id: 1, type: 'Ship 1', message: 'Container Ship', resolved: false},
-    { id: 1, type: 'Ship 1', message: 'Container Ship', resolved: true},
+  // const TicketInfo: TicketInfo[] = [
+  //   { id: 1, type: 'Ship 1', message: 'Container Ship', resolved: false},
+  //   { id: 1, type: 'Ship 1', message: 'Container Ship', resolved: true},
+  //   { id: 1, type: 'Ship 1', message: 'Container Ship', resolved: false},
+  //   { id: 1, type: 'Ship 1', message: 'Container Ship', resolved: false},
+  //   { id: 1, type: 'Ship 1', message: 'Container Ship', resolved: false},
+  //   { id: 1, type: 'Ship 1', message: 'Container Ship', resolved: true},
 
-  ];
+  // ];
   
   const TicketTable: React.FC <TicketInfo> = () => {
     const [ticketInfo, setTicketInfo] = useState<TicketInfo[]>([
@@ -27,17 +27,17 @@ interface TicketInfo {
       { id: 6, type: 'Ship 7', message: 'Container Ship', resolved: true},
     ]);
   
-    const [selectedValue, setSelectedValue] = useState<boolean | null>(null);
+    const [selectedValue, setSelectedValue] = useState<boolean>(false);
   
     const handleUpdateClick = (e: React.MouseEvent<HTMLButtonElement>, ticket: TicketInfo) => {
       setSelectedValue(ticket.resolved);
     };
   
   
-    const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>, ticket: TicketInfo) => {
-      const updatedTicketInfo = ticketInfo.map(t =>
-        t.id === ticket.id ? { ...t, resolved: e.target.value === 'true' } : t);
-      setTicketInfo(updatedTicketInfo);
+    const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>, id: number) => {
+      let updated: TicketInfo = ticketInfo.find(t => t.id === id)!
+      updated.resolved = !updated.resolved
+      setTicketInfo(ticketInfo => [...ticketInfo, updated]);
     };
 
     return (
@@ -55,16 +55,16 @@ interface TicketInfo {
             </tr>
             </thead>
             <tbody>
-            {TicketInfo.map((ticket) => (
+            {ticketInfo.map((ticket) => (
                 <tr key={ticket.id} className="hover:bg-gray-200">
                 <td className="px-4 py-2 text-center">{ticket.id}</td>
                 <td className="px-4 py-2 text-center">{ticket.type}</td>
                 <td className="px-4 py-2 text-center">{ticket.message}</td>
                 <td className="px-4 py-2 text-center">
                 {selectedValue === ticket.resolved ? (
-                  <select value={selectedValue} onChange={(e) => handleSelectChange(e, ticket)}>
-                    <option value={true}>True</option>
-                    <option value={false}>False</option>
+                  <select onChange={(e) => handleSelectChange(e, ticket.id)}>
+                    <option value={'true'}>True</option>
+                    <option value={'false'}>False</option>
                   </select>
                 ) : (
                   ticket.resolved.toString()
@@ -74,8 +74,8 @@ interface TicketInfo {
                 <button onClick={(e) => handleUpdateClick(e, ticket)} className="bg-green-500 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded">Update</button>
                 </td>
                 <td className="px-4 py-2 text-center">
-                <button o
-                // nClick={(e) => handleDeleteClick(e, ticket)} 
+                <button 
+                // onClick={(e) => handleDeleteClick(e, ticket)} 
                 className="bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded">Delete</button>
                 </td>
                 </tr>
